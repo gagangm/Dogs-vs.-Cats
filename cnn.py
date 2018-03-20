dc23bee2b0cd22ef8d27587efc01fd30f7f7dbcf
Gagan G M <gagangm94@gmail.com>
2018-03-20 17:11:06 +0530

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -66,4 +66,4 @@ classifier.fit_generator(training_set,
                          samples_per_epoch = 8000,
                          nb_epoch = 25,
                          validation_data = test_set,
-                         nb_val_samples = 2000)
+                         validation_steps = 2000)
