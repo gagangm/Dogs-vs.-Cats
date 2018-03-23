d97c00d8480dbf03adb1916de9457d2f5b40e9aa
Gagan G M <gagangm94@gmail.com>
2018-03-23 13:30:25 +0530

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -9,6 +9,9 @@
 # Installing Keras
 # pip install --upgrade keras
 
+# Installing Pillow
+# pip install --upgrade pillow
+
 # Part 1 - Building the CNN
 
 # Importing the Keras libraries and packages
@@ -17,6 +20,7 @@ from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+from PILLOW import image
 
 # Initialising the CNN
 classifier = Sequential()
